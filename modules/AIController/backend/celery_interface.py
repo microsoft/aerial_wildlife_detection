6b64fa435e11dd7ccac88d359e7a3fa645c2594b
bkellenb <kudu@kuduvm.5svuschbwnqu5b5t3fbzyv3y5b.xx.internal.cloudapp.net>
2020-04-10 12:58:32 +0000

    [WIP] AIC worker task outsourcing bugfix

--- modules/AIController/backend/celery_interface.py
+++ modules/AIController/backend/celery_interface.py
@@ -7,47 +7,23 @@
 
 import os
 from celery import current_app
-from modules.AIController.backend.middleware import AIMiddleware
+# from modules.AIController.backend.middleware import AIMiddleware
+from modules.AIController.backend.functional import AIControllerWorker
 from util.configDef import Config
 
 
 # init AIController middleware
 modules = os.environ['AIDE_MODULES']
 passiveMode = (os.environ['PASSIVE_MODE']=='1' if 'PASSIVE_MODE' in os.environ else False) or not('aicontroller' in modules.lower())
-aim = AIMiddleware(Config(), passiveMode)
+aim = AIControllerWorker(Config(), current_app)
+# aim = AIMiddleware(Config(), passiveMode)
 
 
-#TODO
-@current_app.task
-def add(x, y):
-    print("Adding {} + {}".format(x, y))
-    return x + y
-@current_app.task(name='Dummy_a')
-def dummy_a(num_workers=1):
-    print("I would split according to {}, but I just return three splits.".format(num_workers))
-    return [[1,2,3],[4,5,6,7],[8,9,10]]
-@current_app.task(name='Dummy_b')
-def dummy_b(imageIDs, index):
-    print("My Index is {}".format(index))
-    print("I received the following image IDs:")
-    print(imageIDs)
-    print("So I return the following subset:")
-    print(imageIDs[index])
-    if index == 1:
-        print("I am a nasty application, I wait 4 extra seconds")
-        import time
-        time.sleep(4)
-    return imageIDs[index],index
-@current_app.task(name='Dummy_c')
-def dummy_c(inputs):
-    print("Dummy C here. I got the following input:")
-    print(inputs)
-    return inputs
 
 
-@current_app.task(name='AIController.aide_internal_notify')
-def aide_internal_notify(message):
-    return aim.aide_internal_notify(message)
+# @current_app.task(name='AIController.aide_internal_notify')
+# def aide_internal_notify(message):
+#     return aim.aide_internal_notify(message)
 
 
 @current_app.task(name='AIController.get_training_images')
@@ -60,25 +36,25 @@ def get_inference_images(project, goldenQuestionsOnly=False, forceUnlabeled=Fals
     return aim.get_inference_images(project, goldenQuestionsOnly, forceUnlabeled, maxNumImages)
 
 
-@current_app.task(name='AIController.start_training')
-def start_training(project, minTimestamp='lastState', minNumAnnoPerImage=0, maxNumImages=None, maxNumWorkers=-1):
-    return aim.start_training(project, minTimestamp, minNumAnnoPerImage, maxNumImages, maxNumWorkers)
+# @current_app.task(name='AIController.start_training')
+# def start_training(project, minTimestamp='lastState', minNumAnnoPerImage=0, maxNumImages=None, maxNumWorkers=-1):
+#     return aim.start_training(project, minTimestamp, minNumAnnoPerImage, maxNumImages, maxNumWorkers)
 
 
-@current_app.task(name='AIController.start_inference')
-def start_inference(project, forceUnlabeled=True, maxNumImages=-1, maxNumWorkers=-1):
-    return aim.start_inference(project, forceUnlabeled, maxNumImages, maxNumWorkers)
+# @current_app.task(name='AIController.start_inference')
+# def start_inference(project, forceUnlabeled=True, maxNumImages=-1, maxNumWorkers=-1):
+#     return aim.start_inference(project, forceUnlabeled, maxNumImages, maxNumWorkers)
 
 
-@current_app.task(name='AIController.inference_fixed')
-def inference_fixed(project, imageIDs, maxNumWorkers=-1):
-    return aim.inference_fixed(project, imageIDs, maxNumWorkers)
+# @current_app.task(name='AIController.inference_fixed')
+# def inference_fixed(project, imageIDs, maxNumWorkers=-1):
+#     return aim.inference_fixed(project, imageIDs, maxNumWorkers)
 
 
-@current_app.task(name='AIController.start_train_and_inference')
-def start_train_and_inference(project, minTimestamp='lastState', minNumAnnoPerImage=0, maxNumImages_train=None, 
-                                    maxNumWorkers_train=1,
-                                    forceUnlabeled_inference=True, maxNumImages_inference=None, maxNumWorkers_inference=1):
-    return aim.start_train_and_inference(project, minTimestamp, minNumAnnoPerImage, maxNumImages_train, 
-                                    maxNumWorkers_train,
-                                    forceUnlabeled_inference, maxNumImages_inference, maxNumWorkers_inference)
+# @current_app.task(name='AIController.start_train_and_inference')
+# def start_train_and_inference(project, minTimestamp='lastState', minNumAnnoPerImage=0, maxNumImages_train=None, 
+#                                     maxNumWorkers_train=1,
+#                                     forceUnlabeled_inference=True, maxNumImages_inference=None, maxNumWorkers_inference=1):
+#     return aim.start_train_and_inference(project, minTimestamp, minNumAnnoPerImage, maxNumImages_train, 
+#                                     maxNumWorkers_train,
+#                                     forceUnlabeled_inference, maxNumImages_inference, maxNumWorkers_inference)
---
+++ modules/AIController/backend/functional.py
@@ -0,0 +1,182 @@
+'''
+    Definition of AIController tasks that are distributed
+    via Celery (e.g. assembling and splitting the lists of
+    training and testing images).
+
+    2020 Benjamin Kellenberger
+'''
+
+from datetime import datetime
+from psycopg2 import sql
+from modules.Database.app import Database
+from util.helpers import array_split
+from .sql_string_builder import SQLStringBuilder
+
+
+
+class AIControllerWorker:
+
+    def __init__(self, config, celery_app):
+        self.config = config
+        self.dbConn = Database(config)
+        self.sqlBuilder = SQLStringBuilder(config)
+        self.celery_app = celery_app
+
+
+    
+    def _get_num_available_workers(self):
+        #TODO: filter for right tasks and queues
+        #TODO: limit to n tasks per worker
+        i = self.celery_app.control.inspect()
+        if i is not None:
+            stats = i.stats()
+            if stats is not None:
+                return len(i.stats())
+        return 1    #TODO
+
+
+
+    def get_training_images(self, project, minTimestamp='lastState', includeGoldenQuestions=True,
+                            minNumAnnoPerImage=0, maxNumImages=None, maxNumWorkers=-1):
+        '''
+            Queries the database for the latest images to be used for model training.
+            Returns a list with image UUIDs accordingly, split into the number of
+            available workers.
+            #TODO: includeGoldenQuestions
+        '''
+        # sanity checks
+        if not (isinstance(minTimestamp, datetime) or minTimestamp == 'lastState' or
+                minTimestamp == -1 or minTimestamp is None):
+            raise ValueError('{} is not a recognized property for variable "minTimestamp"'.format(str(minTimestamp)))
+
+        # identify number of available workers
+        if maxNumWorkers != 1:
+            # only query the number of available workers if more than one is specified to save time
+            num_workers = min(maxNumWorkers, self._get_num_available_workers())
+        else:
+            num_workers = maxNumWorkers
+
+        # query image IDs
+        queryVals = []
+
+        if minTimestamp is None:
+            timestampStr = sql.SQL('')
+        elif minTimestamp == 'lastState':
+            timestampStr = sql.SQL('''
+            WHERE iu.last_checked > COALESCE(to_timestamp(0),
+            (SELECT MAX(timecreated) FROM {id_cnnstate}))''').format(
+                id_cnnstate=sql.Identifier(project, 'cnnstate')
+            )
+        elif isinstance(minTimestamp, datetime):
+            timestampStr = sql.SQL('WHERE iu.last_checked > COALESCE(to_timestamp(0), %s)')
+            queryVals.append(minTimestamp)
+        elif isinstance(minTimestamp, int) or isinstance(minTimestamp, float):
+            timestampStr = sql.SQL('WHERE iu.last_checked > COALESCE(to_timestamp(0), to_timestamp(%s))')
+            queryVals.append(minTimestamp)
+
+        if minNumAnnoPerImage > 0:
+            queryVals.append(minNumAnnoPerImage)
+
+        if maxNumImages is None:
+            limitStr = sql.SQL('')
+        else:
+            limitStr = sql.SQL('LIMIT %s')
+            queryVals.append(maxNumImages)
+
+        if minNumAnnoPerImage <= 0:
+            queryStr = sql.SQL('''
+                SELECT newestAnno.image FROM (
+                    SELECT image, last_checked FROM {id_iu} AS iu
+                    JOIN (
+                        SELECT id AS iid
+                        FROM {id_img}
+                        WHERE corrupt IS NULL OR corrupt = FALSE
+                    ) AS imgQ
+                    ON iu.image = imgQ.iid
+                    {timestampStr}
+                    ORDER BY iu.last_checked ASC
+                    {limitStr}
+                ) AS newestAnno;
+            ''').format(
+                id_iu=sql.Identifier(project, 'image_user'),
+                id_img=sql.Identifier(project, 'image'),
+                timestampStr=timestampStr,
+                limitStr=limitStr)
+
+        else:
+            queryStr = sql.SQL('''
+                SELECT newestAnno.image FROM (
+                    SELECT image, last_checked FROM {id_iu} AS iu
+                    JOIN (
+                        SELECT id AS iid
+                        FROM {id_img}
+                        WHERE corrupt IS NULL OR corrupt = FALSE
+                    ) AS imgQ
+                    ON iu.image = imgQ.iid
+                    {timestampStr}
+                    {conjunction} image IN (
+                        SELECT image FROM (
+                            SELECT image, COUNT(*) AS cnt
+                            FROM {id_anno}
+                            GROUP BY image
+                            ) AS annoCount
+                        WHERE annoCount.cnt >= %s
+                    )
+                    ORDER BY iu.last_checked ASC
+                    {limitStr}
+                ) AS newestAnno;
+            ''').format(
+                id_iu=sql.Identifier(project, 'image_user'),
+                id_img=sql.Identifier(project, 'image'),
+                id_anno=sql.Identifier(project, 'annotation'),
+                timestampStr=timestampStr,
+                conjunction=(sql.SQL('WHERE') if minTimestamp is None else sql.SQL('AND')),
+                limitStr=limitStr)
+
+        imageIDs = self.dbConn.execute(queryStr, tuple(queryVals), 'all')
+        imageIDs = [i['image'] for i in imageIDs]
+
+        if maxNumWorkers > 1:
+            # split for distribution across workers (TODO: also specify subset size for multiple jobs; randomly draw if needed)
+            imageIDs = array_split(imageIDs, max(1, len(imageIDs) // num_workers))
+        else:
+            imageIDs = [imageIDs]
+
+        return imageIDs
+
+
+
+    def get_inference_images(self, project, goldenQuestionsOnly=False, forceUnlabeled=False, maxNumImages=None, maxNumWorkers=-1):
+            '''
+                Queries the database for the latest images to be used for inference after model training.
+                Returns a list with image UUIDs accordingly, split into the number of available workers.
+                #TODO: goldenQuestionsOnly
+            '''
+            if maxNumImages is None or maxNumImages == -1:
+                queryResult = self.dbConn.execute('''
+                    SELECT maxNumImages_inference
+                    FROM aide_admin.project
+                    WHERE shortname = %s;''', (project,), 1)
+                maxNumImages = queryResult['maxnumimages_inference']    
+            
+            queryVals = (maxNumImages,)
+
+            # load the IDs of the images that are being subjected to inference
+            sql = self.sqlBuilder.getInferenceQueryString(project, forceUnlabeled, maxNumImages)
+            imageIDs = self.dbConn.execute(sql, queryVals, 'all')
+            imageIDs = [i['image'] for i in imageIDs]
+
+            # split for distribution across workers
+            if maxNumWorkers != 1:
+                # only query the number of available workers if more than one is specified to save time
+                num_available = self._get_num_available_workers()
+                if maxNumWorkers == -1:
+                    maxNumWorkers = num_available   #TODO: more than one process per worker?
+                else:
+                    maxNumWorkers = min(maxNumWorkers, num_available)
+            
+            if maxNumWorkers > 1:
+                imageIDs = array_split(imageIDs, max(1, len(imageIDs) // maxNumWorkers))
+            else:
+                imageIDs = [imageIDs]
+            return imageIDs
