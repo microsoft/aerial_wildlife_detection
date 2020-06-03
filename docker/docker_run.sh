cb7076493fa7e2019b5d5c6eed08a686963e0086
szjarek <szjarek@outlook.com>
2020-06-03 15:24:47 +0200

    Added script with GPU support for Docker

---
+++ docker/docker_run.sh
@@ -0,0 +1,18 @@
+#docker run --user $(id -u):$(id -g) --gpus '"device=0"' --name aide_cnt --rm -it \
+docker run --name aide_cnt \
+ --rm \
+ -it \
+ -v `pwd`:/home/aide/app \
+ -v aide_db_data:/var/lib/postgresql/10/main \
+ -v aide_images:/home/aide/images \
+ -p 8080:8080 \
+ -h 'aide_app_host' \
+ aide_app
+
+ # Options:
+ # --gpus   - sets GPU configuration
+ # --rm     - forces container removal on close
+ # -it      - interactive mode with tty terminal attached
+ # -v       - maps volume (note: aide_db_data and aide_images needs to be created before this script is executed)
+ # -p       - maps ports
+ # -h       - sets hostname (fixed hostname is required for some internal components)
