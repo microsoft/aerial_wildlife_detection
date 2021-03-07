e6b6641893c6cc53f92920af395693a03f0fddd9
= <=>
2021-03-07 13:22:17 +0000

    reverted database connector; incr. min. conn. to 2

--- modules/Database/app.py
+++ modules/Database/app.py
@@ -1,7 +1,7 @@
 '''
     Database connection functionality.
 
-    2019-21 Benjamin Kellenberger
+    2019-20 Benjamin Kellenberger
 '''
 
 from contextlib import contextmanager
@@ -18,7 +18,7 @@ class Database():
         self.config = config
 
         if verbose_start:
-            print('Connecting to database...'.ljust(LogDecorator.get_ljust_offset()), end='')
+            print('Connecting to database...'.ljust(5), end='')
 
         # get DB parameters
         try:
@@ -43,17 +43,10 @@ class Database():
             LogDecorator.print_status('ok')
 
 
-    def __del__(self):
-        try:
-            self.connectionPool.closeall()
-        except:
-            pass
-
-
     def _createConnectionPool(self):
         self.connectionPool = ThreadedConnectionPool(
-            1,
-            self.config.getProperty('Database', 'max_num_connections', type=int, fallback=20),
+            2,
+            max(2, self.config.getProperty('Database', 'max_num_connections', type=int, fallback=20)),  # 2 connections are needed as minimum for retrying of execution
             host=self.host,
             database=self.database,
             port=self.port,
@@ -81,130 +74,95 @@ class Database():
 
     def execute(self, query, arguments, numReturn=None):
         with self._get_connection() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-                # execute statement
+            cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+            # execute statement
+            try:
+                cursor.execute(query, arguments)
+                conn.commit()
+            except Exception as e:
+                if not conn.closed:
+                    conn.rollback()
+                # self.connectionPool.putconn(conn, close=False)    #TODO: this still causes connection to close
+                conn = self.connectionPool.getconn()
+
+                # retry execution
                 try:
+                    cursor = conn.cursor(cursor_factory=RealDictCursor)
                     cursor.execute(query, arguments)
                     conn.commit()
+                except:
+                    if not conn.closed:
+                        conn.rollback()
+                    print(e)
 
-                    # get results
-                    try:
-                        returnValues = []
-                        if numReturn is None:
-                            # cursor.close()
-                            return
-                        
-                        elif numReturn == 'all':
-                            returnValues = cursor.fetchall()
-                            # cursor.close()
-                            return returnValues
-
-                        else:
-                            for _ in range(numReturn):
-                                rv = cursor.fetchone()
-                                if rv is None:
-                                    return returnValues
-                                returnValues.append(rv)
+            # get results
+            try:
+                returnValues = []
+                if numReturn is None:
+                    # cursor.close()
+                    return
                 
-                            # cursor.close()
-                            return returnValues
-                    except Exception as eGet:
-                        print(eGet)
+                elif numReturn == 'all':
+                    returnValues = cursor.fetchall()
+                    # cursor.close()
+                    return returnValues
 
+                else:
+                    for _ in range(numReturn):
+                        rv = cursor.fetchone()
+                        if rv is None:
+                            return returnValues
+                        returnValues.append(rv)
+        
+                    # cursor.close()
+                    return returnValues
+            except Exception as e:
+                print(e)
+    
+
+    def execute_cursor(self, query, arguments):
+        with self._get_connection() as conn:
+            cursor = conn.cursor(cursor_factory=RealDictCursor)
+            try:
+                cursor.execute(query, arguments)
+                conn.commit()
+                return cursor
+            except:
+                if not conn.closed:
+                    conn.rollback()
+                # cursor.close()
+
+                # retry execution
+                conn = self.connectionPool.getconn()
+                try:
+                    cursor = conn.cursor(cursor_factory=RealDictCursor)
+                    cursor.execute(query, arguments)
+                    conn.commit()
                 except Exception as e:
                     if not conn.closed:
                         conn.rollback()
                     print(e)
-                    # self.connectionPool.putconn(conn, close=False)    #TODO: this still causes connection to close
-                    
-                    # conn = self.connectionPool.getconn()
-
-                    # # retry execution
-                    # try:
-                    #     cursor = conn.cursor(cursor_factory=RealDictCursor)
-                    #     cursor.execute(query, arguments)
-                    #     conn.commit()
-                    # except:
-                    #     if not conn.closed:
-                    #         conn.rollback()
-                    #     print(e)
-        
-
-    # def execute_cursor(self, query, arguments):
-    #     with self._get_connection() as conn:
-    #         with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-    #             try:
-    #                 cursor.execute(query, arguments)
-    #                 conn.commit()
-    #                 yield cursor
-    #             except Exception as e:
-    #                 if not conn.closed:
-    #                     conn.rollback()
-    #                 print(e)
-    #                 # cursor.close()
-
-    #                 # # retry execution
-    #                 # conn = self.connectionPool.getconn()
-    #                 # try:
-    #                 #     cursor = conn.cursor(cursor_factory=RealDictCursor)
-    #                 #     cursor.execute(query, arguments)
-    #                 #     conn.commit()
-    #                 # except Exception as e:
-    #                 #     if not conn.closed:
-    #                 #         conn.rollback()
-    #                 #     print(e)
-    #             finally:
-    #                 if not cursor.closed:
-    #                     cursor.close()
 
 
-    def insert(self, query, values, numReturn=None):
+    def insert(self, query, values):
         with self._get_connection() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor = conn.cursor()
+            try:
+                execute_values(cursor, query, values)
+                conn.commit()
+            except Exception as e:
+                if not conn.closed:
+                    conn.rollback()
+                # cursor.close()
+
+                # retry execution
+                conn = self.connectionPool.getconn()
                 try:
+                    cursor = conn.cursor(cursor_factory=RealDictCursor)
                     execute_values(cursor, query, values)
                     conn.commit()
-
-                    # get results
-                    try:
-                        returnValues = []
-                        if numReturn is None:
-                            # cursor.close()
-                            return
-                        
-                        elif numReturn == 'all':
-                            returnValues = cursor.fetchall()
-                            # cursor.close()
-                            return returnValues
-
-                        else:
-                            for _ in range(numReturn):
-                                rv = cursor.fetchone()
-                                if rv is None:
-                                    return returnValues
-                                returnValues.append(rv)
-                
-                            # cursor.close()
-                            return returnValues
-                    except Exception as eGet:
-                        print(eGet)
-
                 except Exception as e:
                     if not conn.closed:
                         conn.rollback()
                     print(e)
-                    # cursor.close()
-
-                    # # retry execution
-                    # conn = self.connectionPool.getconn()
-                    # try:
-                    #     cursor = conn.cursor(cursor_factory=RealDictCursor)
-                    #     execute_values(cursor, query, values)
-                    #     conn.commit()
-                    # except Exception as e:
-                    #     if not conn.closed:
-                    #         conn.rollback()
-                    #     print(e)
-                finally:
-                    if not cursor.closed:
-                        cursor.close()
