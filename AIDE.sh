f4f5f457c7b3cc2c8fa12ed10aa3857f6cdbcd15
= <=>
2021-03-11 08:56:19 +0000

    launching bug fix; misc. improvements

--- AIDE.sh
+++ AIDE.sh
@@ -12,20 +12,34 @@ function start {
     IFS=',' read -ra ADDR <<< "$AIDE_MODULES"
 
     # Celery
-    numCeleryModules=0;
-    appendMarketplace='';
+    # numCeleryModules=0;
+    # for i in "${ADDR[@]}"; do
+    #     module="$(echo "$i" | tr '[:upper:]' '[:lower:]')";
+    #     if [ "$module" == "labelui" || "$module" == "aiworker" ] || [ "$module" == "fileserver" ]; then
+    #         ((numCeleryModules++));
+    #     fi
+    # done
+    # if [ $numCeleryModules -gt 0 ]; then
+    launchCeleryBeat=false
+    IFS=',' read -ra ADDR <<< "$AIDE_MODULES"
     for i in "${ADDR[@]}"; do
         module="$(echo "$i" | tr '[:upper:]' '[:lower:]')";
-        if [ "$module" == "labelui" ]; then
-            ((numCeleryModules++));
-            appendMarketplace='ModelMarketplace';
-        elif [ "$module" == "aiworker" ] || [ "$module" == "fileserver" ]; then
-            ((numCeleryModules++));
+        if [ "$module" == "fileserver" ]; then
+            folderWatchInterval=$(python util/configDef.py --section=FileServer --parameter=watch_folder_interval --fallback=60);
+            if [ $folderWatchInterval -gt 0 ]; then
+                launchCeleryBeat=true;
+            fi
         fi
     done
-    if [ $numCeleryModules -gt 0 ]; then
-        celery -A celery_worker worker -Q aide_broadcast,$AIDE_MODULES,$appendMarketplace &
+    if [ $launchCeleryBeat ]; then
+        # folder watching interval specified; enable Celery beat
+        tempDir="$(python util/configDef.py --section=FileServer --parameter=tempfiles_dir --fallback=/tmp)/aide/celery/";
+        mkdir -p $tempDir;
+        celery -A celery_worker worker -B -s $tempDir --hostname aide@%h &
+    else
+        celery -A celery_worker worker --hostname aide@%h & 
     fi
+    # fi
 
     # AIDE
     numHTTPmodules=0;
